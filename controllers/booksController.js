4e95f91da4bd8df437ce4d135e35c8f587e412bb
adhilunnikrishnan <adhilunnikrishnanofficial@gmail.com>
2025-11-17 00:09:35 +0530

    feat:edit option correcting

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,10 +1,21 @@
+import { ObjectId } from "mongodb";
+import collection from "../config/collection.js";
+import connectDB from "../config/db.js";
+
 export const editBookDetailsPage = async (req, res) => {
   console.log(">>>>>>>>>>>detailofBook");
   try {
+      const bookId = req.params.id; 
     // Render dashboard
-    res.render("admin/addBook", {
+    const db = await connectDB()
+
+    const bookData = await db.collection(collection.BOOKS_COLLECTION).findOne({_id: new ObjectId(String(bookId))})
+    console.log(bookData)
+
+    res.render("admin/bookDetailsEdit", {
       layout: "admin",
-      title: "Admin - Add Book",
+      title: "Admin - Edit Books Details",
+      bookDetails:bookData
     });
   } catch (error) {
     // console.error("Error loading admin dashboard:", error);
